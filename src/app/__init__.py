b77513dc4bd39816a35406fd7ccf211a729bd893
Gloryness <gravity50ml@gmail.com>
2022-08-01 16:12:24 +0100

    v0.0.2-alpha-03

--- src/app/__init__.py
+++ src/app/__init__.py
@@ -4,7 +4,7 @@ from util.process import convert_to_time
 __title__ = 'app'
 __author__ = 'Gloryness'
 __license__ = 'MIT License'
-__version__ = '0.0.2-alpha-02'
+__version__ = '0.0.2-alpha-03'
 __module__ = os.getcwd()
 
 if not __module__.endswith("app"):
