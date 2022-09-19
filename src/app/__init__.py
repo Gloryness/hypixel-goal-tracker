a6e01463191b35acfc9d8c741990be386420277a
Gloryness <gravity50ml@gmail.com>
2022-09-19 16:55:42 +0100

    Updated to v0.0.2-alpha-08

--- src/app/__init__.py
+++ src/app/__init__.py
@@ -1,10 +1,9 @@
 import os
-from util.process import convert_to_time
 
 __title__ = 'app'
 __author__ = 'Gloryness'
 __license__ = 'MIT License'
-__version__ = '0.0.2-alpha-03'
+__version__ = '0.0.2-alpha-08'
 __module__ = os.getcwd()
 
 if not __module__.endswith("app"):
